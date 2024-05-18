291044e708dbc3a8268526bb5d8eac09260a0893
avivabou <68495475+avivabou@users.noreply.github.com>
2024-05-18 21:59:47 +0300

    Update test _unconstrained_min.py

--- tests/test _unconstrained_min.py
+++ tests/test _unconstrained_min.py
@@ -9,10 +9,10 @@ class TestUnconstrainedMinimization(unittest.TestCase):
         f, grad_f, hessian_f = example(True)
         
         gradient_descent = UnconstrainedMinimization(f, grad_f, method='gradient_descent', max_iter=max_iter)
-        gradient_descent.minimize(x0)
+        gd_iterations, gd_location, gd_is_min = gradient_descent.minimize(x0)
 
         newton = UnconstrainedMinimization(f, grad_f, hessian_f=hessian_f, method='newton')
-        newton.minimize(x0)
+        n_iterations, n_location, n_is_min = newton.minimize(x0)
 
         plot_contour_lines(f,
                            title,
@@ -20,25 +20,36 @@ class TestUnconstrainedMinimization(unittest.TestCase):
                            x_lim,
                            y_lim)
 
-        plot_function_values([(gradient_descent.evaluations, 'Gradient Descent'), (newton.evaluations, 'Newton')], title=f'Function values per Iteration of {title}')
+        plot_function_values([(gradient_descent.evaluations, 'Gradient Descent'), (newton.evaluations, 'Newton')], title=title)
+        
+        print()
+        print("TEST:", title)
+        print("="*(len(title)+5))
+        print()
+        print("  Method   |     Gradient Descent     |     Newton ")
+        print("-----------------------------------------------------")
+        print("Iterations | {:<24} | {:<10}".format(gd_iterations, n_iterations))
+        print("Last X     | {:<23}  | {:<20}".format(str(gd_location), str(n_location)))
+        print("Is Minimum | {:<24} | {:<10}".format(str(gd_is_min), str(n_is_min)))
+        print()
 
     def test_quadratic_example_1(self):
-        self._run_test(examples.quadratic_example_1, title="Quadratic Example 1 Contour Plot")
+        self._run_test(examples.quadratic_example_1, title="Quadratic Example 1")
 
     def test_quadratic_example_2(self):
-        self._run_test(examples.quadratic_example_2, title="Quadratic Example 2 Contour Plot")
+        self._run_test(examples.quadratic_example_2, title="Quadratic Example 2")
 
     def test_quadratic_example_3(self):
-        self._run_test(examples.quadratic_example_3, title="Quadratic Example 3 Contour Plot")
+        self._run_test(examples.quadratic_example_3, title="Quadratic Example 3")
 
     def test_rosenbrock_function(self):
-        self._run_test(examples.rosenbrock_function, x0=np.array([-1.0,2.0]), title="Rosenbrock Function Contour Plot", x_lim=[-2.0,2.0], y_lim=[-2.0,5.0], max_iter=10000)
+        self._run_test(examples.rosenbrock_function, x0=np.array([-1.0,2.0]), title="Rosenbrock Function", x_lim=[-2.0,2.0], y_lim=[-2.0,5.0], max_iter=10000)
 
     def test_linear_function(self):
-        self._run_test(examples.linear_function, title="Linear Function Contour Plot")
+        self._run_test(examples.linear_function, title="Linear Function")
 
     def test_triangle_function(self):
-       self._run_test(examples.triangle_function, title="Triangle Function Contour Plot")
+       self._run_test(examples.triangle_function, title="Triangle Function")
 
 if __name__ == '__main__':
     try:
