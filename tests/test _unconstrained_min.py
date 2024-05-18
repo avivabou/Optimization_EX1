18021c51023de99c30b258c0b5207b23cd0d686b
avivabou <68495475+avivabou@users.noreply.github.com>
2024-05-18 20:10:38 +0300

    Update test _unconstrained_min.py

--- tests/test _unconstrained_min.py
+++ tests/test _unconstrained_min.py
@@ -5,10 +5,10 @@ from src.unconstrained_min import UnconstrainedMinimization
 from src.utils import plot_contour_lines, plot_function_values
 
 class TestUnconstrainedMinimization(unittest.TestCase):
-    def _run_test(self, example, x0, title,  x_lim = [-2.0,2.0], y_lim=[-2.0,2.0]):
+    def _run_test(self, example, title, x0 = [1.0,1.0], x_lim = [-2.0,2.0], y_lim=[-2.0,2.0], max_iter=100):
         f, grad_f, hessian_f = example(True)
         
-        gradient_descent = UnconstrainedMinimization(f, grad_f, method='gradient_descent')
+        gradient_descent = UnconstrainedMinimization(f, grad_f, method='gradient_descent', max_iter=max_iter)
         gradient_descent.minimize(x0)
 
         newton = UnconstrainedMinimization(f, grad_f, hessian_f=hessian_f, method='newton')
@@ -23,22 +23,22 @@ class TestUnconstrainedMinimization(unittest.TestCase):
         plot_function_values([(gradient_descent.evaluations, 'Gradient Descent'), (newton.evaluations, 'Newton')], title=f'Function values per Iteration of {title}')
 
     def test_quadratic_example_1(self):
-        self._run_test(examples.quadratic_example_1, np.array([1.0, 1.0]), title="Quadratic Example 1 Contour Plot")
+        self._run_test(examples.quadratic_example_1, title="Quadratic Example 1 Contour Plot")
 
     def test_quadratic_example_2(self):
-        self._run_test(examples.quadratic_example_2, np.array([1.0, 1.0]), title="Quadratic Example 2 Contour Plot")
+        self._run_test(examples.quadratic_example_2, title="Quadratic Example 2 Contour Plot")
 
     def test_quadratic_example_3(self):
-        self._run_test(examples.quadratic_example_3, np.array([1.0, 1.0]), title="Quadratic Example 3 Contour Plot")
+        self._run_test(examples.quadratic_example_3, title="Quadratic Example 3 Contour Plot")
 
     def test_rosenbrock_function(self):
-        self._run_test(examples.rosenbrock_function, np.array([-1.0, 2.0]), title="Rosenbrock Function Contour Plot", x_lim=[-2.0,2.0], y_lim=[-2.0,5.0])
+        self._run_test(examples.rosenbrock_function, x0=[-1.0, 2.0], title="Rosenbrock Function Contour Plot", x_lim=[-2.0,2.0], y_lim=[-2.0,5.0], max_iter=10000)
 
     def test_linear_function(self):
-        self._run_test(examples.linear_function, np.array([-100.0, 2.0]), title="Linear Function Contour Plot")
+        self._run_test(examples.linear_function, title="Linear Function Contour Plot")
 
     def test_triangle_function(self):
-       self._run_test(examples.triangle_function, np.array([2.0, 2.0]), title="Triangle Function Contour Plot")
+       self._run_test(examples.triangle_function, title="Triangle Function Contour Plot")
 
 if __name__ == '__main__':
     try:
